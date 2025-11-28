4cc8143a16a143be1191b616fad172e7d835d8ec
khodehm <aliteza861@gmail.com>
2025-11-28 05:14:04 +0330

    add:postgres bookstore app styles templates insert - get

--- postgres/main.go
+++ postgres/main.go
@@ -3,64 +3,112 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
+	"html/template"
+	"net/http"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
 
-type Book struct {
-	isbn   string
-	title  string
-	author string
-	price  float32
-}
+var db *sql.DB
+var tpl *template.Template
 
-// connect to postgres database
-// it is local database so you should have postgresql installed
-func main() {
-	// user := os.Getenv("POSTGRES_USER")
-	// db := os.Getenv("POSTGRES_DB")
-	// password := os.Getenv("POSTGRES_PASSWORD")
-	// host := os.Getenv("HOST")
-	// if user == "" || db == "" || password == "" || host == "" {
-	// 	log.Fatalln("Please set the environment variables POSTGRES_USER, POSTGRES_DB, POSTGRES_PASSWORD and HOST")
-	// }
-	// cs := strings.Join([]string{"postgresql://", user, ":", password, "@", host, "/", db, "?sslmode=disable"}, "")
-
-	// connection string
-	db, err := sql.Open("postgres", "postgresql://bookstoremanager:password@localhost/bookstore?sslmode=disable")
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgresql://bookstoremanager:password@localhost/bookstore?sslmode=disable")
 	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
-	err = db.Ping()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Database succesfully connected!")
-	SelectFromBooks(db)
-
-}
-
-func SelectFromBooks(d *sql.DB) {
-	rows, err := d.Query(`select * from books`)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer rows.Close()
-	bks := make([]Book, 0)
-	for rows.Next() {
-		b := Book{}
-		err := rows.Scan(&b.isbn, &b.title, &b.author, &b.price)
-		if err != nil {
-			panic(err)
-		}
-		bks = append(bks, b)
-	}
-	if err = rows.Err(); err != nil {
 		panic(err)
 	}
-	for _, bk := range bks {
-		fmt.Printf("%#v\n", bk)
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println("Database succesfully connected!")
+	tpl = template.Must(template.ParseGlob("template/*"))
+}
+
+type Book struct {
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Price  float32 `json:"price"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+	fileserver := http.FileServer(http.Dir("./assets/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
+	mux.HandleFunc("GET /books/create", createBookPage)
+	mux.HandleFunc("POST /books/create/process", createBookProcess)
+	mux.HandleFunc("/", getBooks)
+	mux.Handle("/favicon.ico", http.NotFoundHandler())
+	mux.HandleFunc("GET /books/show", getBook)
+	http.ListenAndServe(":9090", mux)
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	books := make([]Book, 0)
+
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		bk := Book{}
+		err = rows.Scan(&bk.Isbn, &bk.Title, &bk.Author, &bk.Price)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		books = append(books, bk)
+	}
+	err = tpl.ExecuteTemplate(w, "index.html", books)
+	if err != nil {
+		http.Error(w, "failed to excute template", http.StatusInternalServerError)
+	}
+}
+func getBook(w http.ResponseWriter, r *http.Request) {
+	isbn := r.FormValue("isbn")
+	if isbn == "" {
+		http.Error(w, "missing required fields", http.StatusBadRequest)
+		return
+
+	}
+	row := db.QueryRow("select * from books where isbn = $1", isbn)
+	bk := Book{}
+	err := row.Scan(&bk.Isbn, &bk.Title, &bk.Author, &bk.Price)
+
+	switch {
+	case err == sql.ErrNoRows:
+		http.Error(w, "no record", http.StatusNotFound)
+		return
+	case err != nil:
+		http.Error(w, row.Err().Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, bk)
+}
+func createBookPage(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "create-book.html", nil)
+}
+func createBookProcess(w http.ResponseWriter, r *http.Request) {
+	b := Book{}
+	b.Isbn = r.FormValue("isbn")
+	b.Title = r.FormValue("title")
+	b.Author = r.FormValue("author")
+	p, _ := strconv.ParseFloat(r.FormValue("price"), 32)
+	b.Price = float32(p)
+	if b.Author == " " || b.Isbn == " " || b.Title == " " {
+		http.Error(w, "misiing reqired fields", http.StatusBadRequest)
+		return
+	}
+	_, err := db.Exec("INSERT INTO books (isbn, title, author, price) VALUES($1,$2,$3,$4)", b.Isbn, b.Title, b.Author, b.Price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tpl.ExecuteTemplate(w, "create-book.html", nil)
+
 }
